bdda74bb8d35b3fd5d773e840a538917acaee621
danielsheetrit <danielshitrit022@gmail.com>
2021-09-16 18:17:31 +0300

    deploy to herolu 3

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 'use strict'
 
-const config = require('./config')
-
 const productRoutes = require('./routes/products-routes')
 
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
+const http = require('http').createServer(app)
 
 app.use(express.json())
-app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
 
 app.use('/api', productRoutes.routes)
 
-app.listen(
-    config.port,
-    () => {
-        console.log(`App is listening on port ${config.port}`)
-    } 
-)
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const port = process.env.PORT || 3030
+
+http.listen(port, () => {
+    console.log('Server is running on port: ' + port)
+})
